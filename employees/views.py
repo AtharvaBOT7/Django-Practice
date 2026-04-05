65a00cd6141be60cc1859830bc2aeb98acde5efa
atharvaBOT7 <atharvachundurwar841@gmail.com>
2026-04-05 12:25:12 -0700

    added back to home feature on employee/pk page

--- employees/views.py
+++ employees/views.py
@@ -6,4 +6,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def employee_detail(request, pk):
         employee = get_object_or_404(Employee, pk=pk)
-        return HttpResponse(employee)
+        context = {
+                'employee': employee
+        }
+        return render(request, 'employee_detail.html', context)
